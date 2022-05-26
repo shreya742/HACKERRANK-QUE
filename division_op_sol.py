eb71b3da8ebdc3b75041fa996a93c55cc82e9e91
Sudireddy Sai Shreya <94278880+shreya742@users.noreply.github.com>
2022-05-26 09:26:50 +0530

    Add files via upload

---
+++ division_op_sol.py
@@ -0,0 +1,11 @@
+
+#PROGRAM FOR DIVISION OPERATORS.
+
+#(/) --> printsfloat value as result.
+#(//) --> prints integer value as result.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a//b)
+print(a/b)
